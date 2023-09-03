f9183e93642f86f8f85eea47912583010edc6ee8
Karan Bisht <134302942+Karan-Bisht16@users.noreply.github.com>
2023-09-04 00:10:57 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+const body = document.querySelector('body');
+const leftArrow = document.querySelector('.carousel-control-prev-icon');
+const rightArrow = document.querySelector('.carousel-control-next-icon');
+body.addEventListener('keydown', function(event){
+    if (event.key==='ArrowRight'){
+        rightArrow.click();
+    } else if (event.key==='ArrowLeft'){
+        leftArrow.click();
+    }
+});
+
 function scrollWin() {
     window.scrollTo({
         top: 200,
